a6bdd56d3bacbb0bdc61977fff8e80e299bab04f
Léana 江 <leana.jiang+git@icloud.com>
2024-10-01 22:04:35 +0200

    Q5: Création d'une liste fictive de pokemon

--- src/app/my-component/my-component.component.ts
+++ src/app/my-component/my-component.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-my-component',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class MyComponentComponent {
   id: string = '';
+  pokemons: Array<Pokemon> = [
+    new Pokemon(1, "foo"),
+    new Pokemon(2, "bar"),
+    new Pokemon(3, "baz"),
+  ]
 }
