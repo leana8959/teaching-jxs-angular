cdcf1660cade996bdc3b0a702f7fb87da242a238
Léana 江 <leana.jiang+git@icloud.com>
2024-12-29 21:44:31 +0100

    better button styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
 import { MatSelectModule } from '@angular/material/select'
+import { MatButtonModule } from '@angular/material/button'
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -29,6 +30,7 @@ import { PokeinfoComponent } from './pokeinfo/pokeinfo.component';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+    MatButtonModule,
     ReactiveFormsModule
   ],
   providers: [
