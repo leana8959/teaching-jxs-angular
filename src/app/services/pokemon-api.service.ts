0796e0b2863eaa20fe8311a44a2c2c399b0fd087
Léana 江 <leana.jiang+git@icloud.com>
2024-12-29 22:20:39 +0100

    add typing information to api calls

--- src/app/services/pokemon-api.service.ts
+++ src/app/services/pokemon-api.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { PokemonName } from '../models/pokemons';
+import { PokemonInfoResponse, PokemonName, PokemonsResponse } from '../models/pokemons';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +11,16 @@ export class PokeapiService {
 
   private httpClient: HttpClient = inject(HttpClient);
 
-  getPokemons(): Observable<any> {
+  getPokemons(): Observable<PokemonsResponse> {
     return this
       .httpClient
-      .get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+      .get<PokemonsResponse>("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
   }
 
-  getPokemonInfo(name: PokemonName): Observable<Object> {
-    return this.httpClient.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+  getPokemonInfo(name: PokemonName): Observable<PokemonInfoResponse> {
+    return this
+      .httpClient
+      .get<PokemonInfoResponse>(`https://pokeapi.co/api/v2/pokemon/${name}`)
   }
 
 }
